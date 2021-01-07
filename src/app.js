605dc676cbcc80f59216cf093c890e1e91f556c2
Kasper Kain <41598018+KasperKain@users.noreply.github.com>
2021-01-06 21:38:28 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
+
+const app = express();
+
+const morganOptions = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOptions));
+app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
+
+app.use(bookmarksRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello, boilerplate!');
+});
+
+app.use(errorHandler);
+
+module.exports = app;
